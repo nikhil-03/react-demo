cf5d2a96480f0b6659a742be2472980a73d94ff0
Nikhil <nikhilprakash2012@gmail.com>
2022-11-30 13:10:28 +0530

    First commit

---
+++ build/precache-manifest.27b31a1bbd94b4d6bacecdd8aa616568.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e233f54c5de1bbf1299e914c615f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74bf06b693dbf07fb2a1",
+    "url": "/static/js/2.aa2d590e.chunk.js"
+  },
+  {
+    "revision": "5dd98ce57a39773c16d5",
+    "url": "/static/js/main.9c9969a5.chunk.js"
+  },
+  {
+    "revision": "7ab15ef3f403f1f1b97b",
+    "url": "/static/js/runtime-main.3a838f45.js"
+  }
+]);
